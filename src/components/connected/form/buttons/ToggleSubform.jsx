6361c19633f74ab516fdc2dee624b5646cdd9a3f
Maxime Des Roches <m.desroches@la-grange.ca>
2016-08-04 13:47:16 -0400

    Hide label

--- src/components/connected/form/buttons/ToggleSubform.jsx
+++ src/components/connected/form/buttons/ToggleSubform.jsx
@@ -17,7 +17,7 @@ export class ToggleSubform extends Component {
 	};
 
 	render() {
-		const toggleLabel = this.props.isCollapsed ? <i className="fa fa-eye"></i> : <i className="fa fa-eye-slash"></i>;
+		const toggleLabel = this.props.isCollapsed ? <span><i className="fa fa-eye"></i> Show</span> : <span><i className="fa fa-eye-slash"></i> Hide</span>;
 		return <a className="button-round button-hide-show" onClick={this.props.toggle}>{toggleLabel}</a>;
 		// return <button onClick={this.deleteRecord} className="button-circle-danger-small"><i className="fa fa-remove"></i></button>;
 	}
