bb661f4a7c2221e7b94977c5d943c3b294abf0f9
Martin Vézina <m.vezina@la-grange.ca>
2017-06-19 11:54:39 -0400

    routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 // import createHistory from 'history/lib/createBrowserHistory';
 // import createHistory from 'history/lib/createHashHistory';
 import { Provider } from 'react-redux';
 // import { historyApi } from './config';
-import { Router, Redirect } from 'react-router';
+import { Router, Route, Redirect } from 'react-router';
 import { appHistory, store } from './appHistory';
 
 import routes from './routes';
+import { Freestone } from 'components/connected/Freestone';
 
 // <Router onUpdate={() => window.scrollTo(0, 0)}
 import { setStore } from 'freestone/api';
@@ -17,7 +19,9 @@ ReactDOM.render(
 	<Provider store={store}>
 		<Router history={appHistory}>
 			<Redirect from="/" to="home" />
-			{routes}
+			<Route path="/" component={Freestone}>
+				{routes}
+			</Route>
 		</Router>
 	</Provider>,
 	document.getElementById('root')
