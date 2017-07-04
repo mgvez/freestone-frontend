551970ce3fd533da7c12e76ae53405f583960d72
Maxime Des Roches <m.desroches@la-grange.ca>
2017-07-04 15:32:08 -0400

    troubleshooting

---
+++ src/components/connected/widgets/TroubleshootingButton.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+export class TroubleshootingButton extends Component {
+	static propTypes = {
+		showIcon: React.PropTypes.bool,
+		customClass: React.PropTypes.string,
+	};
+
+	render() {
+		const icon = this.props.showIcon ? <i className="fa fa-gear fa-fw"></i> : null;
+		return (
+			<Link to={'/troubleshooting'} className={`troubleshooting-button ${this.props.customClass}`}>{icon}<span className="nav-label">Troubleshooting</span></Link>
+		);
+	}
+}
