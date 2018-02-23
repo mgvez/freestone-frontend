a2d0157086ad42126c95749e74ae79e793115219
Martin Vézina <m.vezina@la-grange.ca>
2018-02-23 15:34:47 -0500

    build

--- src/components/menu/NavGroup.jsx
+++ src/components/menu/NavGroup.jsx
@@ -5,7 +5,7 @@ import Table from './Table';
 import Module from './Module';
 import Page from './Page';
 import NativeModule from './NativeModule';
-import Collapser from '../../animation/Collapser';
+import Collapsable from '../animation/Collapsable';
 
 export default class NavGroup extends Component {
 	static propTypes = {
@@ -17,73 +17,56 @@ export default class NavGroup extends Component {
 		toggleState: PropTypes.object,
 	};
 
-	constructor(props) {
-		super(props);
-		this.collapser = new Collapser({
-			getOpenState: (fromProps) => {
-				const propsToCheck = fromProps || this.props;
-				return propsToCheck.toggleState[propsToCheck.data.id];
-			},
-			changeState: () => {
-				this.props.toggleCollapse(this.props.data.id);
-			},
-			getContainer: () => {
-				return this._children;
-			},
-		});
-	}
-
-	componentDidUpdate(prevProps) {
-		this.collapser.didUpdate(prevProps);
-	}
-
 	getChildrenGroups(level) {
 		return this.props.data.childrenGroups.map((item) => {
 			return <NavGroup key={item.id} data={item} level={level} toggleState={this.props.toggleState} toggleCollapse={this.props.toggleCollapse} />;
 		});
 	}
 
-	getContents() {
-		if (!this.collapser.getOpenState()) return null;
+	getContents(isOpen) {
 		const level = this.props.level + 1;
-		return (<ul className="sub-nav" ref={(el) => this._children = el}>
-			{
-				this.props.data.tables.map((item) => {
-					return 	<li className="nav-item" key={item.id}><Table name={item.name} id={item.id} displayLabel={item.displayLabel} nrecords={item.nrecords} /></li>;
-				})
-			}
-			{
-				this.props.data.modules.map((item) => {
-					// console.log(item.isFrontendComponent, item);
-					if (item.isFrontendComponent) {
-						return <li className="nav-item" key={`mod-${item.id}`}><NativeModule {...item} /></li>;
-					}
-					return <li className="nav-item" key={`mod-${item.id}`}><Module {...item} /></li>;
-				})
-			}
-			{
-				this.props.data.pages.map((item) => {
-					// console.log(item);
-					return <li className="nav-item" key={`pg-${item.id}`}><Page {...item} /></li>;
-				})
-			}
-			{this.getChildrenGroups(level)}
-		</ul>);
+		return (<Collapsable isCollapsed={isOpen}>
+			<ul className="sub-nav">
+				{
+					this.props.data.tables.map((item) => {
+						return 	<li className="nav-item" key={item.id}><Table name={item.name} id={item.id} displayLabel={item.displayLabel} nrecords={item.nrecords} /></li>;
+					})
+				}
+				{
+					this.props.data.modules.map((item) => {
+						// console.log(item.isFrontendComponent, item);
+						if (item.isFrontendComponent) {
+							return <li className="nav-item" key={`mod-${item.id}`}><NativeModule {...item} /></li>;
+						}
+						return <li className="nav-item" key={`mod-${item.id}`}><Module {...item} /></li>;
+					})
+				}
+				{
+					this.props.data.pages.map((item) => {
+						// console.log(item);
+						return <li className="nav-item" key={`pg-${item.id}`}><Page {...item} /></li>;
+					})
+				}
+				{this.getChildrenGroups(level)}
+			</ul>
+		</Collapsable>);
+	}
+
+	onToggle = () => {
+		this.props.toggleCollapse(this.props.data.id);
 	}
 
 	render() {
-
-		// console.log(this.props.level + ' nav group rendered', this.props.data);
-		const isOpen = this.collapser.getOpenState();
-
+		if (!this.props.data) return null;
+		const isOpen = !this.props.toggleState[this.props.data.id];
 		const activeClass = isOpen ? 'active' : '';
 		const icon = this.props.data.icon || 'folder';
 
-		const contents = this.getContents();
-		// console.log(this.props.data);
+		const contents = this.getContents(isOpen);
+
 		return (
 			<li className={`${activeClass} nav-group`} >
-				<a onClick={this.collapser.toggle} className={`table-group ${activeClass}`}>
+				<a onClick={this.onToggle} className={`table-group ${activeClass}`}>
 					<i className={`fa fa-${icon} fa-fw`}></i>
 					<span className="nav-label">{this.props.data.name}</span> <span className="fa fa-angle-down"></span>
 				</a>
