f002f08930e5cfcd157adec975f89abdbf7d4f77
Maxime Des Roches <m.desroches@la-grange.ca>
2020-05-28 15:01:03 -0400

    bank image div clickable area

--- dist/assets/lagrange/tinymce/freestone/langs/en.js
+++
@@ -1,5 +0,0 @@
-tinyMCE.addI18n('en.vadmin',{
-	descLink : 'Ajouter un lien vers une autre page du site',
-	descImg : 'Ajouter une image de la banque',
-	descDoc : 'Ajouter un document de la banque'
-});
