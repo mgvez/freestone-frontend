1143e5565c943abc5f32068a9d495fecb10297b1
Martin Vézina <m.vezina@la-grange.ca>
2020-12-03 19:49:42 -0500

    list component as functional

--- src/components/recordList/List.jsx
+++ src/components/recordList/List.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import DocumentMeta from 'react-document-meta';
 
@@ -18,161 +18,147 @@ import { Icon } from '../../styles/Icon';
 
 const LARGE_MINW_BREAKPOINT = 1024;
 
-export default class List extends Component {
-	static propTypes = {
+export default function List(props) {
 
-		params: PropTypes.shape({
-			filter: PropTypes.string,
-			page: PropTypes.string,
-			search: PropTypes.string,
-			order: PropTypes.string,
-		}),
+	const [isLarge, setIsLarge] = useState(true);
 
-		tableName: PropTypes.string,
-		table: PropTypes.object,
-		groupedRecords: PropTypes.array,
-		nPages: PropTypes.number,
-		curPage: PropTypes.number,
-		nRecords: PropTypes.number,
-		swappedRecords: PropTypes.object,
-		canAdd: PropTypes.bool,
-		needsFetch: PropTypes.bool,
+	useEffect(() => {
+		const handleResize = () => {
+			const w = window.innerWidth;
+			setIsLarge(w > LARGE_MINW_BREAKPOINT);
+		};
 
-		fetchTable: PropTypes.func,
-		addRecord: PropTypes.func,
-		goTo: PropTypes.func,
+		window.addEventListener('resize', handleResize);
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
 
-	};
-
-	static contextTypes = {
-		router: PropTypes.object,
-	};
-
-	constructor(props) {
-		super(props);
-		this.state = { windowWidth: 0, isLarge: true };
-	}
+	useEffect(() => {
+		if (!props.table) props.fetchTable(props.tableName);
+	});
 	
-	componentDidMount() {
-		this.requireData();
-		window.addEventListener('resize', this.handleResize);
-		this.handleResize();
-	}
+	const getNumRecords = () => {
+		return props.groupedRecords && props.groupedRecords.reduce((total, gr) => total + gr.records.reduce((subtotal) => subtotal + 1, 0), 0);
+	};
 
-	componentDidUpdate() {
-		this.requireData();
-	}
-
-	componentWillUnmount() {
-		window.removeEventListener('resize', this.handleResize);
-	}
-
-	getNumRecords = () => {
-		return this.props.groupedRecords && this.props.groupedRecords.reduce((total, gr) => total + gr.records.reduce((subtotal) => subtotal + 1, 0), 0);
-	}
-
-	requireData() {
-		if (!this.props.table) this.props.fetchTable(this.props.tableName);
-	}
-
-	handleResize = () => {
-		const windowWidth = window.innerWidth;
-		const isLarge = windowWidth > LARGE_MINW_BREAKPOINT;
-		this.setState({ windowWidth, isLarge });
-	}
-
-	addRecord = () => {
-		createRecord(this.props.table).then(res => {
+	const addRecord = () => {
+		createRecord(props.table).then(res => {
 			const { newRecord, newRecordId } = res;
-			this.props.addRecord(this.props.table.id, newRecord);
+			props.addRecord(props.table.id, newRecord);
 
-			const path = `/edit/${this.props.tableName}/${newRecordId}`;
-			this.props.goTo(path);
+			const path = `/edit/${props.tableName}/${newRecordId}`;
+			props.goTo(path);
 		});
-	}
+	};
 
-	render() {
-		// console.log(this.props.swappedRecords);
-		// console.log('render list', this.props.table);
-		// console.log(this.props.searchableFields);
-		let output;
-		let readyToScroll = false;
-		if (this.props.table) {
 
-			const addBtn = this.props.canAdd ? <Button onClick={this.addRecord} round="true"><Icon icon="plus-circle" /> New record</Button> : null;
-			
-			let records = null;
-			// if record list is loaded, display records. Bank records are displayed differently than regular records.
-			if (this.props.groupedRecords) {
-				readyToScroll = true;
+	// console.log(props.swappedRecords);
+	// console.log('render list', props.table);
+	// console.log(props.searchableFields);
+	let output;
+	let readyToScroll = false;
+	if (props.table) {
 
-				if (this.props.table.bankName) {
-					records = (<BankList
-						isLarge={this.state.isLarge}
-						bankName={this.props.table.bankName}
-						{...this.props}
-					/>);
-				} else {
-					records = (<StandardList
-						isLarge={this.state.isLarge}
-						{...this.props}
-					/>);
-				}
+		const addBtn = props.canAdd ? <Button onClick={addRecord} round="true"><Icon icon="plus-circle" /> New record</Button> : null;
+		
+		let records = null;
+		// if record list is loaded, display records. Bank records are displayed differently than regular records.
+		if (props.groupedRecords) {
+			readyToScroll = true;
+
+			if (props.table.bankName) {
+				records = (<BankList
+					isLarge={isLarge}
+					bankName={props.table.bankName}
+					{...props}
+				/>);
+			} else {
+				records = (<StandardList
+					isLarge={isLarge}
+					{...props}
+				/>);
 			}
-
-			const needsFetch = !this.props.groupedRecords || this.props.needsFetch;
-			// console.log('render list needs fetch %s', needsFetch);
-			output = (
-				<section>
-					<DocumentMeta title={`${this.props.table.displayLabel} - list`} />
-
-					<Header>
-						<HeaderTexts columns="8">
-							<Heading1>{this.props.table.displayLabel}</Heading1>
-							<p dangerouslySetInnerHTML={{ __html: this.props.table.help }} />
-						</HeaderTexts>
-						<HeaderFcn columns="3" offset="10" justify="end" align="end">
-							{addBtn}
-						</HeaderFcn>
-					</Header>
-					
-					<TablePermissions table={this.props.table} />
-					<MainZone>
-						
-						<ListSearch 
-							key={`search_${this.props.tableName}`}
-							tableName={this.props.tableName}
-							numRecords={this.getNumRecords()}
-							search={this.props.params.search}
-							goTo={this.props.goTo}
-							needsFetch={needsFetch}
-						>
-							<ListFetch needsFetch={needsFetch} tableName={this.props.tableName} params={this.props.params} />
-						</ListSearch>
-						{this.props.table.isBatchEditable && (
-							<Button warn="true" onClick={() => this.props.goTo(`/batch/${this.props.tableName}`)}>Batch edit</Button>
-						)}
-						<Paging
-							nPages={this.props.nPages}
-							curPage={this.props.curPage}
-							tableName={this.props.tableName}
-						/>
-						{records}
-						<Paging
-							nPages={this.props.nPages}
-							curPage={this.props.curPage}
-							tableName={this.props.tableName}
-						/>
-					</MainZone>
-					
-				</section>
-			);
 		}
 
-		return (
-			<InScroll isReady={readyToScroll}>
-				{output}
-			</InScroll>
+		const needsFetch = !props.groupedRecords || props.needsFetch;
+		// console.log('render list needs fetch %s', needsFetch);
+		output = (
+			<section>
+				<DocumentMeta title={`${props.table.displayLabel} - list`} />
+
+				<Header>
+					<HeaderTexts columns="8">
+						<Heading1>{props.table.displayLabel}</Heading1>
+						<p dangerouslySetInnerHTML={{ __html: props.table.help }} />
+					</HeaderTexts>
+					<HeaderFcn columns="3" offset="10" justify="end" align="end">
+						{addBtn}
+					</HeaderFcn>
+				</Header>
+				
+				<TablePermissions table={props.table} />
+				<MainZone>
+					
+					<ListSearch 
+						key={`search_${props.tableName}`}
+						tableName={props.tableName}
+						numRecords={getNumRecords()}
+						search={props.params.search}
+						goTo={props.goTo}
+						needsFetch={needsFetch}
+					>
+						<ListFetch needsFetch={needsFetch} tableName={props.tableName} params={props.params} />
+					</ListSearch>
+					{props.table.isBatchEditable && (
+						<Button warn="true" onClick={() => props.goTo(`/batch/${props.tableName}`)}>Batch edit</Button>
+					)}
+					<Paging
+						nPages={props.nPages}
+						curPage={props.curPage}
+						tableName={props.tableName}
+					/>
+					{records}
+					<Paging
+						nPages={props.nPages}
+						curPage={props.curPage}
+						tableName={props.tableName}
+					/>
+				</MainZone>
+				
+			</section>
 		);
 	}
+
+	return (
+		<InScroll isReady={readyToScroll}>
+			{output}
+		</InScroll>
+	);
+
 }
+
+List.propTypes = {
+
+	params: PropTypes.shape({
+		filter: PropTypes.string,
+		page: PropTypes.string,
+		search: PropTypes.string,
+		order: PropTypes.string,
+	}),
+
+	tableName: PropTypes.string,
+	table: PropTypes.object,
+	groupedRecords: PropTypes.array,
+	nPages: PropTypes.number,
+	curPage: PropTypes.number,
+	nRecords: PropTypes.number,
+	swappedRecords: PropTypes.object,
+	canAdd: PropTypes.bool,
+	needsFetch: PropTypes.bool,
+
+	fetchTable: PropTypes.func,
+	addRecord: PropTypes.func,
+	goTo: PropTypes.func,
+
+};
