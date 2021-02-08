ad353b9969af6af9a5427dd5f9574f3e29fc9919
Etienne <e.ouellet@la-grange.com>
2021-02-08 16:01:48 +0100

    fix a couple linting errors

--- src/components/nativeModules/textTranslations/TextTranslations.jsx
+++ src/components/nativeModules/textTranslations/TextTranslations.jsx
@@ -261,7 +261,7 @@ export default class TextTranslations extends Component {
 					}
 					
 					return (
-						<Container key={tIdx} style={{marginBottom: "0"}}>
+						<Container key={tIdx} style={{ marginBottom: '0' }}>
 							<GridItem columns="10" offset="1" className="translation">
 								<GridContainer>
 									<GridItem columns="12" align="center" className="translation-label">
@@ -300,7 +300,7 @@ export default class TextTranslations extends Component {
 				<div>
 					{groupsTogglers}
 
-					<div style={{backgroundColor:'#fafafa', marginTop: '-30px', padding: '15px', borderLeft: '1px solid', borderRight: '1px solid', borderBottom: '1px solid', borderColor: '#d4d8d9'}}>
+					<div style={{ backgroundColor: '#fafafa', marginTop: '-30px', padding: '15px', borderLeft: '1px solid', borderRight: '1px solid', borderBottom: '1px solid', borderColor: '#d4d8d9' }}>
 						{groups}
 					</div>
 				</div>
